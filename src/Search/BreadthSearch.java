1b85e5da0d8cec05e3e65d7ef838f6cf1be6e64d
Flambo <flambo.villed@gmail.com>
2020-05-06 19:56:59 -0500

    genere la interfaz de newJFrame, pero aun tengo problemas con mostrar la lista

--- src/Search/BreadthSearch.java
+++ src/Search/BreadthSearch.java
@@ -3,23 +3,36 @@ package Search;
 
 public class BreadthSearch {
     public static void main(String[] args) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrame().setVisible(true);
+            }
+        });
+        
+        
         //static case
         Graph graph = new Graph(6);
-        graph.addVertex("A");
-        graph.addVertex("B");
-        graph.addVertex("C");
-        graph.addVertex("D");
-        graph.addVertex("E");
-        graph.addVertex("F");
+        
+        graph.addVertex("A"); // 0
+        graph.addVertex("B"); // 1
+        graph.addVertex("C"); // 2
+        graph.addVertex("D"); // 3
+        graph.addVertex("E"); // 4
+        graph.addVertex("F"); // 5
+                
+        // nodos adyacentes de A
         graph.addEdge(0,1);
-        graph.addEdge(1,2);
         graph.addEdge(0,3);
-        graph.addEdge(3,4);
-        graph.addEdge(1,4);//
-        graph.addEdge(4,5);
         
+        // nodos adyacentes de B
+        graph.addEdge(1,2);
         
+        // nodos adyacentes de D
+        graph.addEdge(3,4);                        
         
+        // nodos adyacentes de E
+        graph.addEdge(4,5);                                
         
         System.out.println("Visits: ");
         graph.bfs();
--- src/Search/Graph.java
+++ src/Search/Graph.java
@@ -1,58 +1,69 @@
-
 package Search;
 
 import java.util.LinkedList;
 import java.util.Queue;
-
 import java.util.LinkedList;
 import java.util.Queue;
+import javax.swing.DefaultListModel;
 
 public class Graph {
     private Vertex vertexList[];
     private int adjacencyMat[][];
     private int nVerts;
-    private Queue<Integer> q;
-    
+    private Queue<Integer> q;    
 
     public Graph(int ver) {
         vertexList = new Vertex[ver];
         adjacencyMat = new int[ver][ver];
         nVerts = 0;
         q = new LinkedList<Integer>();
-         
-        
-                
     }
-    public void addVertex(String label){
-        vertexList[nVerts++] = new Vertex(label);
-    }
+
+    Graph() { } // constructor
+    
+    public void addVertex(String label){ vertexList[nVerts++] = new Vertex(label); }
+    
     public void addEdge(int start, int end){
         adjacencyMat[start][end] = 1;
         adjacencyMat[end][start] = 1;
-    } 
-    public void displayVertex(int v) {
-        System.out.println(vertexList[v].label);
     }
+    
+    public void displayVertex(int v) { System.out.println(vertexList[v].label); }
     
     public int getAdjacencyMatrix(int v){
         for (int i = 0; i < nVerts; i++) {
             if (adjacencyMat[v][i] == 1 && vertexList[i].visited == false) {
-                System.out.println("Retornó " + adjacencyMat[v][i]);
-                return i;
-                
-            }else{
-                System.out.println("Retornó erroneo " + adjacencyMat[v][i]);
-            }
-            
+                //System.out.println("Retornó " + adjacencyMat[v][i]);                                                
+                return i;                          
+            }            
         }
-        
         return -1;
     }
-    public void bfs(){
+    
+    public DefaultListModel bfs(){                   
+        DefaultListModel model = new DefaultListModel();                     
+        model.addElement(vertexList[0].label);
+        q.add(0);        
+        int v2;
+        
+        while(!q.isEmpty()) {
+            int v1 = q.remove();
+            while((v2 = getAdjacencyMatrix(v1))!= -1){
+                vertexList[v2].visited = true;                                
+                model.addElement(vertexList[v2].label);
+                q.add(v2);                                   
+            }            
+        }  
+        System.out.println(model);
+        System.out.println("se supone que este modelo debe ir en la lista de la "
+                + "/n pantalla al presionar el boton pero solo se imprime");
+        return model;
+    }        
+    
+    public void bfsa(){
         vertexList[0].visited = true;
         displayVertex(0);
-        q.add(0);
-        System.out.println(q.toString());
+        q.add(0);        
         int v2;
         
         while(!q.isEmpty()) {
@@ -70,6 +81,6 @@ public class Graph {
                 System.out.print( adjacencyMat[i][j]+" ");
             }
             System.out.println("");
-        }
+        }                
     }
 }
